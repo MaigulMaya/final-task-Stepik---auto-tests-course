f0efbd698cad83dcc104a779ea7bc056a06c069b
Maya <maigul.mukasheva@gmail.com>
2020-08-24 16:24:33 +0600

    Test login page

---
+++ test_login_page.py
@@ -0,0 +1,25 @@
+from .pages.login_page import LoginPage
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.should_be_login_page()          # выполняем метод страницы - переходим на страницу логина
+
+def test_should_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link) 
+    page.open()                      
+    page.should_be_login_url() 
+
+def test_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link) 
+    page.open()                      
+    page.should_be_login_form() 
+
+def test_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link) 
+    page.open()                      
+    page.should_be_register_form() 
